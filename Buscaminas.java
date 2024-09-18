88c6911d49dbf56ded8d0949749f79434b12b69a
Miguelang <aalmanzaa9928@gmail.com>
2024-09-18 19:54:53 -0400

    Create Buscaminas.java

---
+++ Buscaminas.java
@@ -0,0 +1,27 @@
+public static void main(String []args){
+    System.out.println("HOLA BUSCAMINAS");
+    tablero(8);
+}
+
+
+public static void tablero(int x){
+    int[][] board;
+    board = new int[x][x];
+
+    for(int i=0;i<x;i++){
+        for(int j=0; j< x; j++){
+            board[i][j] = 0;
+
+        }
+    }
+
+    for(int i=0;i<x;i++){
+        for(int j=0; j< x; j++){
+            System.out.print(board[i][j]+" ");
+
+        }
+        System.out.print("\n");
+    }
+
+}
+
