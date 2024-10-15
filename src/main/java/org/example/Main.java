75f84eb647c707e3bc4351f45484e45e49ffd714
Miguelang <202300135@est.umss.edu>
2024-10-14 23:00:17 -0400

    Buscaminas terminado 1.1 solo cambie de dificil a facil porsiacaso

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        args = new String[]{"d=dificil"};
+        args = new String[]{"d=facil"};
         Buscaminas buscaminas = new Buscaminas();
         buscaminas.run(args);
     }
